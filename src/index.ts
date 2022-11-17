f9ab99969ff05c73f59aa597bd7f5950197b3723
Ariful Alam <alamariful1727@gmail.com>
2022-11-16 22:44:02 -0500

    feat: dotenv & config added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+import dotenv = require("dotenv");
 import express from "express";
+import config from "config";
 
 const app = express();
+dotenv.config();
+
+const PORT = config.get("port");
 
 app.get("/", (req, res) => {
 	return res.send("Hello, visitor");
 });
 
-app.listen(4000, () => {
-	console.log(`Server is listening at http://localhost:${4000}`);
+app.listen(PORT, () => {
+	console.log(`Server is listening at http://localhost:${PORT}`);
 });
